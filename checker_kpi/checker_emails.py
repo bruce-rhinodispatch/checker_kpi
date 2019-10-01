99f1e3b70d8f8e6785cb4efde902e070228d2863
denisolyanyuk <denisolyanyuk@gmail.com>
2019-10-01 12:02:02 -0400

    Update checker_emails.py

--- checker_kpi/checker_emails.py
+++ checker_kpi/checker_emails.py
@@ -2,12 +2,32 @@ import base64
 import pickle
 import os
 import re
+from time import sleep
+
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from pprint import pprint
 import datetime
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+global_variable_for_emails = []
+
+def check_message(request_id, response, exception):
+    global global_variable_for_emails
+    if exception is None:
+        global_variable_for_emails.append({'response': response, 'id': request_id})
+    else:
+        print(exception)
+
+
+def get_body_from_part(part):
+    if part['mimeType'] == 'text/plain' or part['mimeType'] == 'multipart/alternative':
+        if part['body']['size'] != 0:
+            encoded_body = part['body']['data']
+            decoded_body = base64.urlsafe_b64decode(encoded_body).decode()
+            return decoded_body
+    return ''
+
 
 def get_creds():
     scopes = ['https://www.googleapis.com/auth/gmail.readonly']
@@ -34,7 +54,7 @@ class CheckerEmails:
             raise Exception(f"Credentials are for {email_creds} but expected for {email}")
         self._email = email
         self._labels_not_to_check = ['CHAT']
-        self._regexp = re.compile(r"(On [A-Z][a-z]{2}, [A-Z][a-z]{2} (\d){1,2}, (\d){4} at (\d){1,2}:\d\d (AM|PM) (.|\s){0,70}@(.|\s){0,70}wrote:)")
+        self._regexp = re.compile(r"(On [A-Z][a-z]{2}, [A-Z][a-z]{2} \d{1,2}, \d{4} at \d{1,2}:\d{2} (AM|PM) (.|\s){0,70}@(.|\s){0,70}wrote:)|([А-я]{2}, \d{1,2} [А-я]+\.? \d{4} г\.,? в? ?\d{1,2}:\d{1,2},? (.|\s){0,70}@.+\.)|(---------- Forwarded message ---------)")
 
     @staticmethod
     def get_body_from_part(part):
@@ -45,45 +65,57 @@ class CheckerEmails:
                 return decoded_body
         return ''
 
+
     def check_emails_by_dispatchers(self, dispatchers, dates):
+        global global_variable_for_emails
         query = f"from:me -label:({' OR '.join(self._labels_not_to_check)}) " \
                 f"after:{dates['start'].strftime('%Y/%m/%d/')} before:{dates['end'].strftime('%Y/%m/%d/')}"
         mails_raw = self._service.users().messages().list(userId='me', q=query).execute()
         # если ничего не нашло
         if mails_raw['resultSizeEstimate'] == 0:
+            print(f'nothing for {self._email}')
             return dispatchers
         estimate_emails = mails_raw["resultSizeEstimate"]
         checked = 0
         # и теперь в цыкле достаем из списка писем котороые подходят по query ид каждого письма
         # и посылаем запрос что бы получить всю информацию по нужному письму
         while True:
+            batch = self._service.new_batch_http_request(callback=check_message)
             for mail_raw in mails_raw['messages']:
                 mail_id = mail_raw['id']
-                message = self._service.users().messages().get(userId='me', id=mail_id, format='full').execute()
+                mail = self._service.users().messages().get(userId='me', id=mail_id, format='full')
+                batch.add(request=mail, request_id=mail_id)
 
+            # изменяет mails через nonlocal
+            batch.execute()
+
+            # проверяем каждое письмо в батче
+            for message in global_variable_for_emails:
                 # TODO: убрать, сейчас для получения ид письма на котором все рухнуло
-                for header in message['payload']['headers']:
+                for header in message['response']['payload']['headers']:
                     if header['name'] == 'Message-ID':
                         message_id_for_gui = header['value']
+                if message_id_for_gui is None:
+                    raise Exception(f'message without message_id_for_gui:{message}')
 
                 # проверяю если емейл был отправлен с нужной мне почты
-                for header in message['payload']['headers']:
+                for header in message['response']['payload']['headers']:
                     if header['name'] == 'From':
                         try:
                             email_from = header['value'].split('<')[1].split('>')[0]
                         except:
-                            print(header['value'])
+                            print(f"message_id_for_gui {message_id_for_gui}")
+                            print(f"header {header['value']}")
 
                 if email_from != self._email:
                     continue
-
                 body = ""
                 # если часть в письме 1
-                if 'parts' not in message['payload']:
-                    body = self.get_body_from_part(message['payload'])
+                if 'parts' not in message['response']['payload']:
+                    body = self.get_body_from_part(message['response']['payload'])
                 # если много частей в письме
                 else:
-                    for part in message['payload']['parts']:
+                    for part in message['response']['payload']['parts']:
                         if 'parts' not in part:
                             partial_body = self.get_body_from_part(part)
                             body += partial_body
@@ -94,52 +126,75 @@ class CheckerEmails:
                                 partial_body = self.get_body_from_part(sub_part)
                                 body += partial_body
 
-
                 if body != "":
                     cleared_body = ""
-                    # херня какая-то, если не читстить руками перед регекспом - думает пол дня
-                    for line in body.split('\r\n'):
-                        if len(line) != 0:
-                            if line[0] != '>':
-                                cleared_body += line+"\r\n"
-
+                    #херня какая-то, если не читстить руками перед регекспом - думает пол дня (решил)
+                    # for line in body.split('\r\n'):
+                    #     if len(line) != 0:
+                    #         if line[0] != '>':
+                    #             cleared_body += line + "\r\n"
+                    cleared_body = body
+                    # if message['id'] == '16b8f1a147345d49':
+                    #     print('gotcha')
                     # убираем цитату в теле письма
                     reg_exp_obj = self._regexp.search(cleared_body)
                     if reg_exp_obj is not None:
                         start_of_quote = reg_exp_obj.start()
                         cleared_body = cleared_body[:start_of_quote]
                     # тут у нас уже есть текст письма, который отправил диспетчер
+                    # print("===" * 25)
+                    # print(f"message_id_for_gui {message_id_for_gui}")
+                    # print(f'message id gmail api {message["id"]}')
+                    # print(cleared_body)
+
                     # и проверяем
                     for dispatcher in dispatchers:
                         if dispatcher['nick'] in cleared_body:
                             dispatcher['amount'] += 1
+
             checked += len(mails_raw['messages'])
+            global_variable_for_emails = []
             print(f"checked {checked} from {estimate_emails}")
             # если больше нет листов для проверки - на выход
             if 'nextPageToken' not in mails_raw:
                 break
-            # если есть - потворяем
+            # если есть - повторяем
             page_token = mails_raw['nextPageToken']
             mails_raw = self._service.users().messages().list(userId='me', pageToken=page_token, q=query).execute()
 
 
 
 
+
+
+
+
 if __name__ == "__main__":
-    from pprint import pprint
     with open('creds.pickle', 'rb') as token:
         creds_pickl = pickle.load(token)
-    creds = creds_pickl['bruce@rhinodispatch.com']
-    emails = 0
-    service = build('gmail', 'v1', credentials=creds)
-    query = f"(from:me) (-label:CHAT) (after:2019/05/10) (before:2019/09/28)"
-    mails_raw = service.users().messages().list(userId='me', q=query).execute()
-    print(f'estimate {mails_raw["resultSizeEstimate"]}')
-    while 'nextPageToken' in mails_raw:
-        emails += len(mails_raw['messages'])
-        page_token = mails_raw['nextPageToken']
-        mails_raw = service.users().messages().list(userId='me', pageToken=page_token, q=query).execute()
-        print(emails)
-    print(f"final result {emails}")
-    batch = service.new_batch_http_request(callback=callback)
-    batch.execute()
+    creds = creds_pickl['tracking1@deltaexpressinc.com']
+    checker = CheckerEmails(creds, 'tracking1@deltaexpressinc.com')
+    names = ["Nick", "Bruce", "Craig"]
+    dispatchers = [{'nick': dispatcher, 'amount': 0} for dispatcher in names]
+    dates = {'start': datetime.datetime.now()-datetime.timedelta(days=2), "end": datetime.datetime.now()+datetime.timedelta(days=1)}
+    checker.check_emails_by_dispatchers(dispatchers, dates)
+    # service = build('gmail', 'v1', credentials=creds)
+    # mail_id = '16b8f1a147345d49'
+    # mail = service.users().messages().get(userId='me', id=mail_id, format='full').execute()
+    #
+    # body = ""
+    # # если часть в письме 1
+    # if 'parts' not in mail['payload']:
+    #     body = get_body_from_part(mail['payload'])
+    # # если много частей в письме
+    # else:
+    #     for part in mail['payload']['parts']:
+    #         if 'parts' not in part:
+    #             partial_body = get_body_from_part(part)
+    #             body += partial_body
+    #
+    #         # если и у частей есть части : собираем все декодируем и собираем в кучу
+    #         else:
+    #             for sub_part in part['parts']:
+    #                 partial_body = get_body_from_part(sub_part)
+    #                 body += partial_body
