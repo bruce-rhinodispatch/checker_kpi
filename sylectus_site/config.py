fd8ca88bd2ea1a1f6de5d83dd742f1c6bbb166bf
denisolyanyuk <denisolyanyuk@gmail.com>
2019-11-12 15:35:27 -0500

    Create config.py

---
+++ sylectus_site/config.py
@@ -0,0 +1,7 @@
+google_config = {'token_uri': 'https://accounts.google.com/o/oauth2/token',
+                    'client_id': '1002690293400-61rg5e6dfc6hraa9mredn7gvcj96vlts.apps.googleusercontent.com',
+                    'client_secret': 'N_0UxADewCx8paVyUJnzA44h',
+                    'scopes': ["openid", "https://www.googleapis.com/auth/userinfo.profile",
+                "https://www.googleapis.com/auth/user.emails.read",
+                "https://www.googleapis.com/auth/gmail.readonly",
+                "https://www.googleapis.com/auth/userinfo.email"]}
