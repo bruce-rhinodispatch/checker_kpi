25ccaa2a01922a2351e8ab3ef613dbf2b957b17f
denisolyanyuk <denisolyanyuk@gmail.com>
2019-11-12 15:35:34 -0500

    Create routing.py

---
+++ sylectus_site/routing.py
@@ -0,0 +1,19 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.conf.urls import url
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
+from sylectus_site.consumers import EmailsConsumer
+
+application = ProtocolTypeRouter({
+    # Empty for now (http->django views is added by default)
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                [
+                    url(r'socket/', EmailsConsumer)
+                ]
+            )
+        )
+    )
+})
+
