2c1a3d679362312a01f97f9ba4ce6652df42c8ce
denisolyanyuk <denisolyanyuk@gmail.com>
2019-11-12 15:34:55 -0500

    Update forms.py

--- checker_kpi/forms.py
+++ checker_kpi/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 
 class DateForm(forms.Form):
-    start = forms.DateTimeField(input_formats=['%m/%d/%Y'])
-    end = forms.DateTimeField(input_formats=['%m/%d/%Y'])
+    start = forms.DateTimeField(input_formats=['%m/%d/%Y'], required=True)
+    end = forms.DateTimeField(input_formats=['%m/%d/%Y'], required=True)
     start.widget.attrs.update({'id': 'start', 'placeholder': "Select start date", 'autocomplete': 'off'})
     end.widget.attrs.update({'id': 'end', 'placeholder': "Select end date", 'autocomplete': 'off'})
 
