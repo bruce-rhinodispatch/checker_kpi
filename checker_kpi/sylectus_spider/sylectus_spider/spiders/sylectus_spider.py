9e0993431bb24c078bbb500f24b8847f79952891
denisolyanyuk <denisolyanyuk@gmail.com>
2019-11-21 07:16:35 -0500

    Update sylectus_spider.py

--- checker_kpi/sylectus_spider/sylectus_spider/spiders/sylectus_spider.py
+++ checker_kpi/sylectus_spider/sylectus_spider/spiders/sylectus_spider.py
@@ -1,27 +1,23 @@
 import datetime
-import pickle
-import pprint
-
 import scrapy
-from scrapy.crawler import CrawlerProcess
 from scrapy.http import FormRequest
 
 
 class SylectusSpider(scrapy.Spider):
-    def __init__(self, date_start, date_end, corporate_id, user_name, user_pass):
-        self._corporate_id = corporate_id
-        self._user_name = user_name
-        self._user_pass = user_pass
-        self._date_start = date_start
-        self._date_end = date_end
-        scrapy.Spider.__init__(self)
-
     name = "sylectus_spider"
     start_urls = [
         'https://www.sylectus.com/Login.aspx'
     ]
-    def parse(self, response):
 
+    def modify_realtime_request(self, request):
+        self._corporate_id = request.meta["corporate_id"]
+        self._user_name = request.meta["user_name"]
+        self._user_pass = request.meta["user_pass"]
+        self._date_start = request.meta["date_start"]
+        self._date_end = request.meta["date_end"]
+        return request
+
+    def parse(self, response):
         viewstate = response.css('[id="__VIEWSTATE"]::attr(value)').extract_first()
         viewstategenerator = response.css('[id="__VIEWSTATEGENERATOR"]::attr(value)').extract_first()
         eventvalidation = response.css('[id="__EVENTVALIDATION"]::attr(value)').extract_first()
@@ -35,17 +31,17 @@ class SylectusSpider(scrapy.Spider):
         }, callback=self.second_login)
 
     def second_login(self, response):
+        #TODO проверить привильный ли ид
         user_ids = response.css('[id="ctl00_bodyPlaceholder_userList"] > option::attr(value)').extract()
         user_names = response.css('[id="ctl00_bodyPlaceholder_userList"] > option::text').extract()
         if len(user_names) != len(user_ids):
-            raise Exception("We have unequal amount of users and their ids")
+            return {"Error": "We have unequal amount of users and their ids"}
         users = {}
         for i in range(len(user_names)):
             users[user_names[i]] = user_ids[i]
-
         user_id = users.get(self._user_name, None)
         if user_id is None:
-            raise Exception("Check the user name")
+            return {"Error": "Something wrong with company's credentials"}
 
         viewstate = response.css('[id="__VIEWSTATE"]::attr(value)').extract_first()
         viewstategenerator = response.css('[id="__VIEWSTATEGENERATOR"]::attr(value)').extract_first()
@@ -62,7 +58,7 @@ class SylectusSpider(scrapy.Spider):
     def logged_in(self, response):
         title = response.css('title::text').extract_first().strip()
         if title != "Sylectus - Trucking's Most Powerful Network":
-            raise Exception("Something is wrong, probably ID and password combination is invalid")
+            return {"Error": f"Probably invalid password for {self._user_name}"}
         self._server_url = response.url.split('/sylectus.asp')[0]
         return FormRequest(self._server_url+"/II05_translogs.asp", method="POST",
                                          callback=self.trans_log,
@@ -71,14 +67,14 @@ class SylectusSpider(scrapy.Spider):
                                              'todate': self._date_end,
                                          }, )
 
-    @staticmethod
-    def trans_log(response):
+    def trans_log(self, response):
         rows = response.css('[action="II05_translogs.asp"]>center>table[width="100%"][cellpadding="1"]>tr')
         actions_by_user = {}
         for row in rows:
             user = row.css('td:nth-child(3)::text').extract_first()
             if user is not None:
                 user = user.strip()
+
             action = row.css('td:nth-child(8)::text').extract_first()
             if action is not None:
                 action = action.strip().replace(" ", "_").replace(".", "").replace("#", "").replace("  ", " ")
@@ -95,27 +91,4 @@ class SylectusSpider(scrapy.Spider):
                 total = 0
             actions_by_user[user]['Total'] = total + 1
             actions_by_user[user][action] = amount + 1
-        with open('actions.pkl', 'wb') as file:
-            pickle.dump(actions_by_user, file)
-        print('Done')
-
-    # @staticmethod
-    # def write_to_html(response):
-    #     with open('response.html', 'w') as file:
-    #         file.write(response.text)
-
-if __name__ == "__main__":
-    import subprocess
-    # (self, date_start, date_end, corporate_id, user_name, user_pass):
-    dates = {'start': datetime.datetime.now() - datetime.timedelta(days=30), 'end': datetime.datetime.now()}
-    start = dates['start'].strftime('%m/%d/%Y')
-    end = dates['end'].strftime('%m/%d/%Y')
-    corporate_id = "26140"
-    user_name = 'UUpdat'
-    user_pass = "Del2029"
-    direcotry = "C:\\Users\\Dispatcher\\Python\\sylectus_site\\checker_kpi\\sylectus_spider\\sylectus_spider\\spiders"
-    pipe = subprocess.call(["scrapy", "crawl", "sylectus_spider", "-a", f"date_start={start}", "-a", f"date_end={end}", "-a", f"corporate_id={corporate_id}",
-                           "-a", f"user_name={user_name}", "-a", f"user_pass={user_pass}"], cwd=direcotry)
-
-    with open('actions.pkl', 'rb') as file:
-        result = pickle.load(file)
+        return actions_by_user
